2bd269038bd5fde3b2410bdd1915e2b0147488f4
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-24 23:28:11 +0400

    added tour component

--- src/Projects/Tours/Components/Tour.jsx
+++ src/Projects/Tours/Components/Tour.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
-const Tour = () => {
+const Tour = ({ id, image, info, name, price, filterTour }) => {
   return (
     <article className="single-tour">
-      <img src={{}} alt={{}} />
+      <img src={image} alt={name} />
       <footer>
         <div className="tour-info">
-          <h4>{}</h4>
-          <h4 className="tour-price"></h4>
+          <h4>{info}</h4>
+          <h4 className="tour-price">${price}</h4>
         </div>
         <p>
           <button></button>
         </p>
-        <button className="delete-btn">not interested</button>
+        <button className="delete-btn" onClick={() => filterTour(id)}>
+          not interested
+        </button>
       </footer>
     </article>
   );
