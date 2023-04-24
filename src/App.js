25d7c0e0d50515c31549f109dbffe0745d321acb
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-24 21:24:42 +0400

    its not good idea

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Loading from "./components/Loading";
+
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <Loading />
     </div>
   );
 }
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Loading = () => {
+  const [x, setX] = useState(1);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setX(x + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [x]);
   return (
-    <div>Loading...</div>
-  )
-}
+    <div>
+      Loading...
+      <h1>{x}</h1>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
