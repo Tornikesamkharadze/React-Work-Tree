e6de8e525be5bc44744e6cb774ae3f9cee5f742a
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-24 21:15:51 +0400

    small project tour

---
+++ src/components/Tours.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Tour from "./Tour";
+const Tours = () => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        <Tour />
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
