091c39c656500b84230ca62428ba8646829da354
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-25 23:11:42 +0400

    start review project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FetchUsers from "./workTree/FetchGithUsers/FetchUsers";
 import InitList from "./Projects/BirthDayReminder/InitList";
 import MultipleReturns from "./workTree/Conditional-Rendering/MultipleReturns";
 import MainTours from "./Projects/Tours/MainTours";
+import MainReview from "./Projects/Reviews/MainReview";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/*  <FetchUsers /> */}
       {/*  <InitList /> */}
       {/* <MultipleReturns /> */}
-      <MainTours />
+      {/* <MainTours /> */}
+      <MainReview />
     </>
   );
 }
---
+++ src/Projects/Reviews/MainReview.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Review from "./Review";
+import "./index.css"
+
+const MainReview = () => {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+};
+
+export default MainReview;
