426070c604d0278a0c09be8ab061e915ae8c1072
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-25 23:45:33 +0400

    added slider function

--- src/Projects/Reviews/Review.jsx
+++ src/Projects/Reviews/Review.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./Data/data";
 
 const Review = () => {
-  return <div>Review</div>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = data[index];
+
+  const checkNumber = (number) => {
+    if (number > data.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return data.length - 1;
+    }
+    return number;
+  };
+  const nextPerson = () => {
+    setIndex((index) => checkNumber(index + 1));
+  };
+  const prevPerson = () => {
+    setIndex((index) => checkNumber(index - 1));
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">{/*   <FaQuoteRight /> */}</span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          Prev Person
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          Next Person
+        </button>
+      </div>
+      {/* <button className='random-btn' onClick={randomPerson}>
+    surprise me
+  </button> */}
+    </article>
+  );
 };
 
 export default Review;
