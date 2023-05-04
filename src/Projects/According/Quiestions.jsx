7de3b1060ecda67c0c721f8027bb021fac6c2d2e
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-04 23:25:06 +0400

    display questions on toggle

--- src/Projects/According/Quiestions.jsx
+++ src/Projects/According/Quiestions.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
-const Quiestions = () => {
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Quiestions = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  const toggleInfo = () => setShowInfo(!showInfo);
   return (
-    <div>Quiestions</div>
-  )
-}
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={toggleInfo}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
+};
 
-export default Quiestions
+export default Quiestions;
