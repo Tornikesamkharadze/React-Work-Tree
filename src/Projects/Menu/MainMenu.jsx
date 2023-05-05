ef35de9e87ec5f0005821ccbef771d657b063da5
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-05 23:49:43 +0400

    added mainmenu component get data and give menu component as a props

--- src/Projects/Menu/MainMenu.jsx
+++ src/Projects/Menu/MainMenu.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import items from "./data";
+import Categories from "./Categories";
+import Menu from "./Menu";
+import "./index.css";
 
 const MainMenu = () => {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
   return (
-    <div>MainMenu</div>
-  )
-}
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline" />
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+};
 
-export default MainMenu
+export default MainMenu;
