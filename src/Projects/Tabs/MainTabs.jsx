fa859bc502ef3a7c9b5ecd489c235368b9b30306
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-06 21:52:35 +0400

    fetch jobs data from api

--- src/Projects/Tabs/MainTabs.jsx
+++ src/Projects/Tabs/MainTabs.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
+const url = "https://course-api.com/react-tabs-project";
 const MainTabs = () => {
-  return (
-    <div>MainTabs</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
 
-export default MainTabs
+  const fetchJobs = async () => {
+    const { data } = await axios(url);
+    setJobs(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  console.log(jobs);
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  return <div>MainTabs</div>;
+};
+
+export default MainTabs;
