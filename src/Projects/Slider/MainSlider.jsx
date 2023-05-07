4c3e6d04d202b398aeebad18f42e766fc476d01b
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-07 23:17:20 +0400

    added react icons

--- src/Projects/Slider/MainSlider.jsx
+++ src/Projects/Slider/MainSlider.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import data from "./data";
 
 const MainSlider = () => {
-  return <div>MainSlider</div>;
+  const [people, setPleople] = useState(data);
+  const [index, setIndex] = useState(0);
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default MainSlider;
