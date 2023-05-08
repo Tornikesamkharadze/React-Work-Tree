633c7f30cff78a763e6da3d17ad4a580a5e0af0c
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-08 18:36:36 +0400

    save data in states

--- src/workTree/Controlled-Inputs/ControlledInput.jsx
+++ src/workTree/Controlled-Inputs/ControlledInput.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import "./index.css";
 
 const ControlledInput = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmali] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <article>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <div className="form-control">
             <label htmlFor="firstName">Name : </label>
-            <input type="text" name="firstName" id="firstName" />
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="email">Email : </label>
-            <input type="email" name="email" id="email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmali(e.target.value)}
+            />
           </div>
           <button type="submit">add person</button>
         </form>
