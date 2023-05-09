3196f35902eecd3c43ba585e95844467253af2ae
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-09 23:20:11 +0400

    added form inpunt

--- src/Projects/Lorem-Ipsum/MainLorem.jsx
+++ src/Projects/Lorem-Ipsum/MainLorem.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import text from "./data";
+import "./index.css";
 
 const MainLorem = () => {
-  return (
-    <div>MainLorem</div>
-  )
-}
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
-export default MainLorem
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("shemovida");
+  };
+
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>lorem</p>
+        <p>lorem</p>
+      </article>
+    </section>
+  );
+};
+
+export default MainLorem;
