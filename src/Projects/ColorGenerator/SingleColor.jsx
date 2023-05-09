a35cf4b721aacfeb5145838a1a3059ec548adf87
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-10 00:44:48 +0400

    added weignt and hexcolor

--- src/Projects/ColorGenerator/SingleColor.jsx
+++ src/Projects/ColorGenerator/SingleColor.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
   return (
-    <div>SingleColor</div>
-  )
-}
+    <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexColor}</p>
+    </article>
+  );
+};
 
-export default SingleColor
+export default SingleColor;
