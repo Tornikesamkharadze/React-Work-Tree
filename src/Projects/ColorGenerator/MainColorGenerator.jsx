9ce58999f54be5ad0a6067b03a15af2f78d79de6
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-10 00:02:59 +0400

    added states

--- src/Projects/ColorGenerator/MainColorGenerator.jsx
+++ src/Projects/ColorGenerator/MainColorGenerator.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from "values.js";
+import "./index.css";
 
 const MainColorGenerator = () => {
-  return (
-    <div>MainColorGenerator</div>
-  )
-}
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState();
 
-export default MainColorGenerator
+  const handleChange = (e) => setColor(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={handleChange}
+            placeholder="#f15025"
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+  );
+};
+
+export default MainColorGenerator;
