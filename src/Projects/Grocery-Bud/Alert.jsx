4944a929adace12efe3a3bc103c4b98e4b925b48
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-13 00:07:35 +0400

    change useffect dependancy array

--- src/Projects/Grocery-Bud/Alert.jsx
+++ src/Projects/Grocery-Bud/Alert.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from "react";
 
-const Alert = ({ msg, type, showAlert }) => {
+const Alert = ({ msg, type, showAlert, list }) => {
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       showAlert();
     }, 3000);
 
     return () => clearTimeout(timeoutId);
-  }, []);
-  
+  }, [list]);
+
   return <p className={`alert alert-${type}`}>{msg}</p>;
 };
 
