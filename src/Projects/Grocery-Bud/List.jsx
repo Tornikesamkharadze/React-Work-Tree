403096f862ec298838429a6a582b4d8e7ac3ea3c
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-12 22:20:54 +0400

    map and display our list

--- src/Projects/Grocery-Bud/List.jsx
+++ src/Projects/Grocery-Bud/List.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 
-const List = () => {
+const List = ({ list }) => {
   return (
-    <div>List</div>
-  )
-}
+    <div className="grocery-list">
+      {list.map((item) => {
+        return (
+          <article className="grocery-item" key={item.id}>
+            <p className="title">{item.title}</p>
+            <div className="btn-container">
+              <button type="button" className="edit-btn">
+                <FaEdit />
+              </button>
+              <button type="button" className="delete-btn">
+                <FaTrashAlt />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
 
-export default List
+export default List;
