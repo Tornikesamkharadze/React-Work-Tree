7db181b6b3502728c363ccbb0e2bb0e3ce4fd345
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-15 21:09:41 +0400

    created html basic structure for nav

--- src/Projects/Navbar/MainNavbar.jsx
+++ src/Projects/Navbar/MainNavbar.jsx
@@ -1,8 +1,55 @@
 import React from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+
 import "./index.css";
+import logo from "./logo.svg";
 
 const MainNavbar = () => {
-  return <div>MainNavbar</div>;
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container show-container">
+          <ul className="links">
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+            <li>
+              <a href="#">products</a>
+            </li>
+          </ul>
+        </div>
+        <ul className="social-icons">
+          <li>
+            <a href="https://twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default MainNavbar;
