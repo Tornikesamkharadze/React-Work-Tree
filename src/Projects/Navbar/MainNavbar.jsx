3662712db2a59985bdaef5234fed4ba3b2170898
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-15 22:31:52 +0400

    added useEffect for check links height using useref and getboundingclientrect

--- src/Projects/Navbar/MainNavbar.jsx
+++ src/Projects/Navbar/MainNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaBars, FaTwitter } from "react-icons/fa";
 
 import { links, social } from "./data";
@@ -8,8 +8,20 @@ import "./index.css";
 
 const MainNavbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
   const toggleBurgerManu = () => setShowLinks(!showLinks);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className="nav-center">
@@ -19,12 +31,8 @@ const MainNavbar = () => {
             <FaBars />
           </button>
         </div>
-        <div
-          className={`${
-            showLinks ? "links-container show-container" : "links-container"
-          }`}
-        >
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
