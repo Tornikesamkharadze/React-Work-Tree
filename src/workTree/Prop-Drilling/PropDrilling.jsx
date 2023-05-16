84a9d4025f1e819d52045d434dd2ac5fe08457a3
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-16 22:07:22 +0400

    added remove person propdrilling ex

--- src/workTree/Prop-Drilling/PropDrilling.jsx
+++ src/workTree/Prop-Drilling/PropDrilling.jsx
@@ -10,26 +10,42 @@ const data = [
 const PropDrilling = () => {
   const [people, setPeople] = useState(data);
 
+  const removePerson = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
+  };
   return (
     <section>
       <h3>Prop Drilling</h3>
-      <List people={people} />
+      <List people={people} removePerson={removePerson} />
     </section>
   );
 };
 
-const List = ({ people }) => {
+const List = ({ people, removePerson }) => {
   return (
     <>
       {people.map((person) => {
-        return <SinglePerson key={person.id} name={person.name} />;
+        return (
+          <SinglePerson
+            key={person.id}
+            {...person}
+            removePerson={removePerson}
+          />
+        );
       })}
     </>
   );
 };
 
-const SinglePerson = ({ name }) => {
-  return <h1>{name}</h1>;
+const SinglePerson = ({ id, name, removePerson }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <button onClick={() => removePerson(id)}>Remove</button>
+    </div>
+  );
 };
 
 export default PropDrilling;
