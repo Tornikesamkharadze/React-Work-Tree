dd38459aa1531014ea78b57aae84d84fc5393110
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-17 23:51:02 +0400

    added buttons for open modal and sidebar

--- src/Projects/Sidebar-Modal/Home.jsx
+++ src/Projects/Sidebar-Modal/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { FaBars } from "react-icons/fa";
+import { useAppContext } from "./context";
 
 const Home = () => {
+  const { openSidebar, openModal } = useAppContext();
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars />
       </button>
-      <button className="btn">show modal</button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
     </main>
   );
 };
