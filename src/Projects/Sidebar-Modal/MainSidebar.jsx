e5ba7f8e90fbf8dc8b6dc08af1b9177b9bd5365e
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-17 23:01:10 +0400

    added components

--- src/Projects/Sidebar-Modal/MainSidebar.jsx
+++ src/Projects/Sidebar-Modal/MainSidebar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Home from "./Home";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+import "./index.css"
 
 const MainSidebar = () => {
-  return <div>MainSidebar</div>;
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  );
 };
 
 export default MainSidebar;
