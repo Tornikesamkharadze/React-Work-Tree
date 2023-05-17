15b1ba518622748417b8658b937ef409d7b6ba0c
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-17 23:30:08 +0400

    wrapp components in appProvider

--- src/Projects/Sidebar-Modal/MainSidebar.jsx
+++ src/Projects/Sidebar-Modal/MainSidebar.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import Home from "./Home";
 import Modal from "./Modal";
 import Sidebar from "./Sidebar";
-import "./index.css"
+import { AppProvider } from "./context";
+import "./index.css";
 
 const MainSidebar = () => {
   return (
     <>
-      <Home />
-      <Modal />
-      <Sidebar />
+      <AppProvider>
+        <Home />
+        <Modal />
+        <Sidebar />
+      </AppProvider>
     </>
   );
 };
