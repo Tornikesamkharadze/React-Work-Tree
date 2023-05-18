3dd8fd5afdfff6a919ef3689e6d37c754f4d4fee
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-18 23:34:17 +0400

    stripesubmenu clone project

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
       {/* <MainUseReducer /> */}
       {/* <PropDrilling /> */}
       {/*  <ContextApi /> */}
-      <MainSidebar />
+      {/* <MainSidebar /> */}
     </>
   );
 }
