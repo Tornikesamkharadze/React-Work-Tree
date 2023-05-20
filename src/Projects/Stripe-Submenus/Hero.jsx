f706d0197c79356d21344a15d88edb51c5166c6d
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-20 22:18:32 +0400

    added html structure

--- src/Projects/Stripe-Submenus/Hero.jsx
+++ src/Projects/Stripe-Submenus/Hero.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useAppContext } from "./context";
+import phoneImg from "./images/phone.svg";
 
 const Hero = () => {
+  const { closeSubmenu } = useAppContext();
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Manage Any Content Anywhere</h1>
+          <p>
+            Strapi is the leading open-source headless CMS. It’s 100% JavaScript
+            and fully customizable.
+          </p>
+          <button className="btn">start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
