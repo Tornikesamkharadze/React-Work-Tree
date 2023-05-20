e161823e54f4c728355cf3ab6d95b7a985850bd2
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-20 23:59:32 +0400

    added setpages state

--- src/Projects/Stripe-Submenus/context.jsx
+++ src/Projects/Stripe-Submenus/context.jsx
@@ -1,32 +1,41 @@
-import { useContext, useState } from "react";
-
+import React, { useContext, useState } from "react";
+import sublinks from "./data";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState();
-  const [isModalOpen, setIsModalOpen] = useState(true);
-
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: "", links: [] });
   const openSidebar = () => {
     setIsSidebarOpen(true);
   };
+
   const closeSidebar = () => {
     setIsSidebarOpen(false);
   };
-  const openModal = () => {
-    setIsModalOpen(true);
+
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
   };
-  const closeModal = () => {
-    setIsModalOpen(false);
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
   };
   return (
     <AppContext.Provider
       value={{
-        isModalOpen,
+        isSubmenuOpen,
         isSidebarOpen,
         openSidebar,
         closeSidebar,
-        openModal,
-        closeModal,
+        openSubmenu,
+        closeSubmenu,
+        location,
+        page,
       }}
     >
       {children}
