22efe1b1cb0642e24a79f1971c00e5fdaaf0ec29
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-20 23:35:22 +0400

    added submenu component

--- src/Projects/Stripe-Submenus/Submenu.jsx
+++ src/Projects/Stripe-Submenus/Submenu.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { useAppContext } from "./context";
 
 const Submenu = () => {
-  return (
-    <div>Submenu</div>
-  )
-}
+  const { isSubmenuOpen, location } = useAppContext();
+  const container = useRef(null);
 
-export default Submenu
+  useEffect(() => {
+    const submenu = container.current;
+    const { bottom, center } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+    console.log(submenu);
+  }, [location]);
+  
+  return (
+    <aside
+      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
+      ref={container}
+    >
+      submenu
+    </aside>
+  );
+};
+
+export default Submenu;
