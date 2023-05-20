86eb3d70f0a296b5677fe4c592cff2a2dc574320
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-20 23:59:04 +0400

    added colums if statement

--- src/Projects/Stripe-Submenus/Submenu.jsx
+++ src/Projects/Stripe-Submenus/Submenu.jsx
@@ -1,24 +1,45 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useAppContext } from "./context";
 
 const Submenu = () => {
-  const { isSubmenuOpen, location } = useAppContext();
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useAppContext();
   const container = useRef(null);
-
+  const [columns, setColumns] = useState("");
   useEffect(() => {
+    setColumns("col-2");
     const submenu = container.current;
     const { bottom, center } = location;
     submenu.style.left = `${center}px`;
     submenu.style.top = `${bottom}px`;
-    console.log(submenu);
-  }, [location]);
-  
+
+    if (links.length === 3) {
+      setColumns("col-3");
+    }
+    if (links.length === 4) {
+      setColumns("col-4");
+    }
+  }, [location, links]);
+
   return (
     <aside
       className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
       ref={container}
     >
-      submenu
+      <h4>{page}</h4>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a key={index} href={url}>
+              {icon} {label}
+            </a>
+          );
+        })}
+      </div>
     </aside>
   );
 };
