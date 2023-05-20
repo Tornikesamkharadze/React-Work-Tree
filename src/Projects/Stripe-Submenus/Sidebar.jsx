75dd5f2aa1c38c9ae78f494f59d89afb2e2de2ca
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-20 22:46:04 +0400

    maping pages and links also added close sidebar

--- src/Projects/Stripe-Submenus/Sidebar.jsx
+++ src/Projects/Stripe-Submenus/Sidebar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import sublinks from "./data";
+import { FaTimes } from "react-icons/fa";
+import { useAppContext } from "./context";
 
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useAppContext();
   return (
-    <div>Sidebar</div>
-  )
-}
+    <sidebar
+      className={`${
+        isSidebarOpen ? "sidebar-wrapper show" : "sidebar-wrapper"
+      }`}
+    >
+      <div className="sidebar">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((item, index) => {
+            const { page, links } = item;
+            return (
+              <article key={index}>
+                {page}
+                <div className="sidebar-sublinks">
+                  {links.map((link, index) => {
+                    const { url, icon, label } = link;
+                    return (
+                      <a key={index} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </sidebar>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
