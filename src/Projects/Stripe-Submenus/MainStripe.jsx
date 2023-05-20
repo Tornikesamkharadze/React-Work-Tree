f5727919c87198bad02eb2d075ece33dde266e4b
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-20 23:32:37 +0400

    wrapped components in approvider

--- src/Projects/Stripe-Submenus/MainStripe.jsx
+++ src/Projects/Stripe-Submenus/MainStripe.jsx
@@ -3,14 +3,18 @@ import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Hero from "./Hero";
 import Submenu from "./Submenu";
+import { AppProvider } from "./context";
+import "./index.css";
 
 const MainStripe = () => {
   return (
     <>
-      <Navbar />
-      <Sidebar />
-      <Hero />
-      <Submenu />
+      <AppProvider>
+        <Navbar />
+        <Sidebar />
+        <Hero />
+        <Submenu />
+      </AppProvider>
     </>
   );
 };
