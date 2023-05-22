07c8632612e4bb4a2749782328afd822640dad77
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-22 23:46:57 +0400

    added html structure

--- src/Projects/Cart/MainCart.jsx
+++ src/Projects/Cart/MainCart.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useGlobalContext } from "./context";
+import CartContainer from "./CartContainer";
+import Navbar from "./Navbar";
 
 const MainCart = () => {
+  const { loading } = useGlobalContext();
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
   return (
-    <div>MainCart</div>
-  )
-}
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+};
 
-export default MainCart
+export default MainCart;
