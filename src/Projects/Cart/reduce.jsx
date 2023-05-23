f56792ea127d24e5c0d4f4305146c12cbcdea9d1
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-23 22:28:18 +0400

    added logic for increase and decrease items

--- src/Projects/Cart/reduce.jsx
+++ src/Projects/Cart/reduce.jsx
@@ -9,22 +9,16 @@ const reducer = (state, action) => {
       cart: state.cart.filter((item) => item.id !== action.payload),
     };
   }
-
-  if (action.type === "INCREASE") {
-    let tempCart = state.cart.map((cartItem) => {
-      if (cartItem.id === action.payload) {
-        return { ...cartItem, amount: cartItem.amount + 1 };
-      }
-      return cartItem;
-    });
-    return { ...state, cart: tempCart };
-  }
-
-  if (action.type === "DECREASE") {
+  if (action.type === "TOGGLE_AMOUNT") {
     let tempCart = state.cart
       .map((cartItem) => {
-        if (cartItem.id === action.payload) {
-          return { ...cartItem, amount: cartItem.amount - 1 };
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === "inc") {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          }
+          if (action.payload.type === "dec") {
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          }
         }
         return cartItem;
       })
@@ -55,9 +49,11 @@ const reducer = (state, action) => {
   if (action.type === "LOADING") {
     return { ...state, loading: true };
   }
+
   if (action.type === "DISPLAY_ITEMS") {
     return { ...state, loading: false, cart: action.payload };
   }
+
   return state;
 };
 
