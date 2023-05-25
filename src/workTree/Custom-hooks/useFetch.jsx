4f8bde2323ef339783dc0c197ad41c74b8cfc5b1
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-25 22:37:17 +0400

    created custom hook

--- src/workTree/Custom-hooks/useFetch.jsx
+++ src/workTree/Custom-hooks/useFetch.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-const useFetch = () => {
-  return <div>useFetch</div>;
+const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  const getProducets = async () => {
+    const respons = await fetch(url);
+    const data = await respons.json();
+    setProducts(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getProducets();
+  }, [url]);
+
+  return { loading, products };
 };
 
 export default useFetch;
