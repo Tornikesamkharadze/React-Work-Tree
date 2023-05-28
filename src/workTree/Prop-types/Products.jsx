ab11206d1f3ec5c092eeaee3d5b6b238f92821e1
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-28 23:20:59 +0400

    proptypes with custome hook

---
+++ src/workTree/Prop-types/Products.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import defaultImage from "../../assets/default-image.jpeg";
+
+const Product = ({ image, name, price }) => {
+  return <article className="product">Single product</article>;
+};
+
+export default Product;
