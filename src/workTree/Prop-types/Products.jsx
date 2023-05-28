458de1de36418a2bf066d4104651ec742f2c5700
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-28 23:28:16 +0400

    handle undefined error

--- src/workTree/Prop-types/Products.jsx
+++ src/workTree/Prop-types/Products.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import defaultImage from "../../assets/default-image.jpeg";
 
 const Product = ({ image, name, price }) => {
-  return <article className="product">Single product</article>;
+  return (
+    <article className="product">
+      <img src={image.url} alt={name} />
+      <h4>{name}</h4>
+      <p>{price}</p>
+    </article>
+  );
 };
 
 export default Product;
