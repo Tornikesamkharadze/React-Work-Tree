a723ce9aaca468ee8a15638efe0188c9c1cd55ca
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-29 22:22:42 +0400

    memo ls

---
+++ src/workTree/memo-useMemo-useCallback/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useCallback, useMemo } from "react";
+import useFetch from "../Custom-hooks/useFetch";
+
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/javascript-store-products";
+
+// every time props or state changes, component re-renders
+/* const expensive = (data) => {
+  return (
+    data.reduce((total, item) => {
+      const price = item.fields.price;
+      if (price >= total) {
+        total = price;
+      }
+      return total;
+    }, 0) / 100
+  );
+};
+ */
+const Index = () => {
+  const { products } = useFetch(url);
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Count : {count}</h1>
+      <button className="btn" onClick={() => setCount(count + 1)}>
+        click me
+      </button>
+      <h1 style={{ marginTop: "3rem" }}>Cart: </h1>
+      <h1>Most expensive: </h1>
+      <BigList products={products} />
+    </>
+  );
+};
+
+const BigList = React.memo(({ products }) => {
+  return (
+    <section className="products">
+      {products.map((product) => {
+        return <SingleProduct key={product.id} {...product} />;
+      })}
+    </section>
+  );
+});
+
+const SingleProduct = ({ fields }) => {
+  let { name, price } = fields;
+  price = price / 100;
+  const image = fields.image[0].url;
+  return (
+    <article className="product">
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>${price}</p>
+      <button>add cart</button>
+    </article>
+  );
+};
+export default Index;
