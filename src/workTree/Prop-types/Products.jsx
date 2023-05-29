f67648e82643ff86fff71e9627ce7f8c5f3e7e13
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-29 18:37:20 +0400

    added proptypes and defaultprops

--- src/workTree/Prop-types/Products.jsx
+++ src/workTree/Prop-types/Products.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import defaultImage from "../../assets/default-image.jpeg";
+import PropTypes from "prop-types";
 
 const Product = ({ image, name, price }) => {
+  const url = image && image.url;
   return (
     <article className="product">
-      <img src={image.url} alt={name} />
+      single product
+      <img src={url || defaultImage} alt={name} />
       <h4>{name}</h4>
-      <p>{price}</p>
+      <p>{price || 3.99}</p>
     </article>
   );
 };
 
+Product.prototype = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+/* Product.defaultProps = {
+  image: defaultImage,
+  name: "default name",
+  price: 3.99,
+};
+ */
 export default Product;
