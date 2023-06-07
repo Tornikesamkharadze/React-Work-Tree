0ba8b642fc503fef2c247f77380bdc2173f7f8d8
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-06-07 23:22:57 +0400

    link home

--- src/workTree/React-router-dom/Pages/About.jsx
+++ src/workTree/React-router-dom/Pages/About.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className="section">
+      <Link to="/" className="btn">
+        Back Home
+      </Link>
+    </section>
+  );
+};
 
-export default About
+export default About;
