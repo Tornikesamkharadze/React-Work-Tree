e5bcb85995396024e20a95352b743293d7f05016
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-06-12 18:13:24 +0400

    remnoved content

--- src/workTree/React-router-dom/Pages/About.jsx
+++ src/workTree/React-router-dom/Pages/About.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <section className="section">
+      <h2>About page</h2>
       <Link to="/" className="btn">
         Back Home
       </Link>
