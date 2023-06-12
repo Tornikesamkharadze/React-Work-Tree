2dc4a210730d56cfa02084eed36cbe08888fbaef
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-06-12 20:02:26 +0400

    refactored

--- src/workTree/React-router-dom/Components/StyledNavbar.jsx
+++ src/workTree/React-router-dom/Components/StyledNavbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const StyledNavbar = () => {
+  /* დიფოლტად NavLink ი ისედაც ადებს კლასნეიმს active-ს მარა 
+  მეორენაირადაც შეგვიძლია გავაკეთოთ თუ აქტიური რა მოხდეს თუ არა რა
+  რიგირც მაქვს ჰოუმის ლინკზე მაგალითად */
   return (
-    <div>StyledNavbar</div>
-  )
-}
+    <nav className="nav">
+      <NavLink
+        to="/"
+        className={({ IsActive }) => (IsActive ? "link active" : "link")}
+      >
+        home
+      </NavLink>
+      <NavLink to="/about">about</NavLink>
+      <NavLink to="/products">products</NavLink>
+    </nav>
+  );
+};
 
-export default StyledNavbar
+export default StyledNavbar;
