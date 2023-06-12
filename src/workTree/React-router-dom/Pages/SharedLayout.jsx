5d7af7ddb80525131ebf684fbaab9ae55225fd63
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-06-12 20:02:40 +0400

    refactored

--- src/workTree/React-router-dom/Pages/SharedLayout.jsx
+++ src/workTree/React-router-dom/Pages/SharedLayout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import StyledNavbar from "../Components/StyledNavbar";
 import { Outlet } from "react-router-dom";
 
 const SharedLayout = () => {
   return (
     <>
-      <Navbar />
+      <StyledNavbar />
       <Outlet />
     </>
   );
