8e83cfc082d59020e97740002c606b2e51cc0dce
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-07-08 17:39:33 +0400

    map products from local data

--- src/workTree/React-router-dom/Pages/Products.jsx
+++ src/workTree/React-router-dom/Pages/Products.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import products from "../Data/data";
+import { Link } from "react-router-dom";
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <section className="setion">
+      <h4>products</h4>
+      <div className="products">
+        {products.map((product) => {
+          return (
+            <article key={product.id}>
+              <h5>{product.name}</h5>
+              <img src={product.image} />
+              <Link to={`/products/${product.id}`}>See product</Link>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Products
+export default Products;
