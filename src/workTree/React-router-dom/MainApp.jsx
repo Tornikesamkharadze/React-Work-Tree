5bdf9be55feceb619cdec63db94ecf6dccce1674
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-07-08 18:06:28 +0400

    added login route

--- src/workTree/React-router-dom/MainApp.jsx
+++ src/workTree/React-router-dom/MainApp.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Products from "./Pages/Products";
 import Error from "./Pages/Error";
 import SingleProduct from "./Pages/SingleProduct";
+import Login from "./Pages/Login";
+import Dashboard from "./Pages/Dashboard";
 import "./index.css";
 import SharedLayout from "./Pages/SharedLayout";
 
 const MainApp = () => {
+  const [user, setUser] = useState();
   return (
     <BrowserRouter>
       <Routes>
@@ -17,6 +20,8 @@ const MainApp = () => {
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
           <Route path="products/:productId" element={<SingleProduct />} />
+          <Route path="login" element={<Login setUser={setUser} />} />
+          <Route path="dashboard" element={<Dashboard user={user} />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
