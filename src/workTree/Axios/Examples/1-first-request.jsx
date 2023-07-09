3c3bd6de63ff5b5c9d97c3351756c9baa0f64831
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-07-09 16:12:24 +0400

    get ref from axios

--- src/workTree/Axios/Examples/1-first-request.jsx
+++ src/workTree/Axios/Examples/1-first-request.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+
+const url = "https://course-api.com/react-store-products";
 
 const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      const respons = await axios(url);
+      const data = respons.data;
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return <div>First Request</div>;
 };
 
